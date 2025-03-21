5ba5f10c8ca92441a071220c31149f514162ef65
rojinakarki413 <146530783+rojinakarki413@users.noreply.github.com>
2025-03-21 16:15:56 +0545

    inversions.java

---
+++ inversions.java
@@ -0,0 +1,20 @@
+public class Inversions{
+   public int countInversion{int[] arr){
+     int count = 0;
+     int n=arr.length;
+       for(int i=0; i<n; i++){
+         for(int j=i+1; j<n; j++){
+           if (arr[i]>arr[j]){
+             count++;
+           }
+         }
+       }
+     return count:
+       }
+
+public static void main(String [] args){
+  int [] array={4,2,1};
+  int inversions=countInversion(array);
+     System.out.println("Total number of inersions:"+inversions);
+   }
+}
